213cf72e1c0430100e648c23b67b86f0059e215c
Cao Yang <caoyangkaka@guest-wireless-207-151-226-178.usc.edu>
2013-04-02 13:20:24 -0700

    p2_1
    added the hashset implement solution

--- p2_1.java
+++ p2_1.java
@@ -1,5 +1,6 @@
 package com.mattcao.p2_1;
 
+import java.util.HashSet;
 /**
  * Created with IntelliJ IDEA.
  * User: caoyangkaka
@@ -18,7 +19,7 @@ package com.mattcao.p2_1;
  * 3, a head and tail pointer included. so we can access/add the first/last in a time O(1)
  *    list.addFirst(), list.add(), listRemoveFirst(), listRemoveLast(), getFirst(), getLast();
  * 4, traverse the list:
- *    ListIterator<String> iter = list.listIterator();
+ *    ListIterator<String> iter = list.ListIterator();
  *    while (iter.hasNext()) {
  *      String word = iter.next();
  *      System.out.println(word);
@@ -34,11 +35,11 @@ package com.mattcao.p2_1;
  */
 public class p2_1 {
     public static void main(String[] args) {
-        Node list = new Node(10);
+        Node list = new Node(14);
         for(int i = 0; i < 15; i++) {
             list.add(i);
         }
-        removeDup(list);
+        removeDupA(list);
         list.display();
     }
 // the no buffer one function
@@ -56,11 +57,42 @@ public class p2_1 {
             list = list.next;
         }
     }
-
+/**
+ * Here we will use the hashmap to do the mapping, but firstly, we should go overview with the hashmap:
+ *        Set and Map
+ *        Set ADT: add only the unduplicated value, so here, we can use this method.
+ *        functions:
+ *        1. Set<elementType> s = new hashSet<elementType>();  do not care about the sorted order
+ *        when iterate
+ *        notice: the elementType need support equals() and hashCode()
+ *        2. Set<elementType> s = new treeSet<elementType>();  care about the sorted order when
+ *        iterate
+ *        notice: elementType must implement Comparable (has compareTo)
+ *        3.
+ *        s.add(12), return true if success, return false if not
+ *        s.remove(12), the same
+ *        s.contains(12), the same
+ *        4. traverse
+ *        Iterator<String> iter = s.iterator();
+ *
+ *
+ */
 // the having buffer function
     public static void removeDupA(Node list) {
-        //use the hashmap, please review this part
+        //1. use hashSet
+        HashSet<Integer> s = new HashSet<Integer>();
+        s.add(list.data);
+        while(list.next != null) {
+            if(s.add(list.next.data)){
+                list = list.next;
+            } else {
+                list.next = list.next.next;
+            }
+        }
+        if(!s.add(list.data))
+            list = null;
     }
+//still here, we can still use the map to do this part.
 }
 
 
